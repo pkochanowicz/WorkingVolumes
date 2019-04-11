6c27dc2d5561d07975923932b0de7c699da46a53
Fransua92 <pkochanowicz92@gmail.com>
2019-04-11 18:51:41 +0200

    fixed mixed content error

--- scripts.js
+++ scripts.js
@@ -43,11 +43,12 @@ function handleScrollDown(itemsList) {
 }
 
 function add_book_to_books_list(item){
-  if (typeof(item.volumeInfo.imageLinks) == 'undefined') var imageAdress = "Pictures/Brak.png";
-  else var imageAdress = item.volumeInfo.imageLinks.thumbnail;
+  if (typeof(item.volumeInfo.imageLinks) == 'undefined') var imageAddress = "Pictures/Brak.png";
+  else var imageAddress = item.volumeInfo.imageLinks.thumbnail;
+  imageAddress = preventMixedContent(imageAddress);
   document.getElementById("books_list").innerHTML += "<div class = 'book_piece'" +
                                                        "<br> <div class ='book_title_label'><b> " + item.volumeInfo.title + "</b> </div>" +
-                                                       "<br> <div class='book_thumbnail'><img src=" + imageAdress + " alt=" + item.volumeInfo.title + "> </div>" +
+                                                       "<br> <div class='book_thumbnail'><img src=" + imageAddress + " alt=" + item.volumeInfo.title + "> </div>" +
                                                        "<br> <div class ='book_description'>" + shorten_string(item.volumeInfo.description, 100) + "</div>" +
                                                      "</div> <br>";
 
@@ -56,14 +57,14 @@ function add_book_to_books_list(item){
 function load_searching_script(adjusted_search_input)
 {
   var searching_scripts_div= document.getElementById("searching_scripts");
-  var current_request_adress = document.getElementById("current_request_adress");
-  if (current_request_adress != null)
+  var current_request_address = document.getElementById("current_request_address");
+  if (current_request_address != null)
   {
     searching_scripts_div.removeChild(searching_scripts_div.childNodes[0]);
   }
   var script= document.createElement("script");
   script.src= "https://www.googleapis.com/books/v1/volumes?q=" + adjusted_search_input +"&callback=handleResponse";
-  script.id = "current_request_adress";
+  script.id = "current_request_address";
   searching_scripts_div.appendChild(script);
 }
 
@@ -73,4 +74,9 @@ function shorten_string(str, maxLen, separator = ' ') {
   return str.substr(0, str.lastIndexOf(separator, maxLen));
 }
  else return "Brak opisu";
+}
+
+function preventMixedContent(link)
+{
+  return link.replace("http", "https");
 }
