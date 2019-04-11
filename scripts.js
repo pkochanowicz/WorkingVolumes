c0262a0b63e18574606348be154d0a59c6a882a9
Fransua92 <pkochanowicz92@gmail.com>
2019-04-11 13:26:33 +0200

    book list now expands on scrolling down

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+var book_items_shown = 0;
+var itemsList;
+
 function findDOMElements(){
   var search_input = document.getElementById("search_input");
   var search_request = document.getElementById("search_request");
@@ -5,24 +8,38 @@ function findDOMElements(){
 
   search_input.onchange = function search(){
   var adjusted_search_input = search_input.value.split(' ').join('+');
-  if (adjusted_search_input != "") load_searching_script(adjusted_search_input);
+  if (adjusted_search_input != "") {
+  load_searching_script(adjusted_search_input);
+  document.getElementById("books_list").innerHTML = "";
+  book_items_shown = 0;
 }
+}
+
+window.onscroll = function() {
+    var pageHeight = document.getElementById("content").clientHeight ;
+    var windowHeight = window.innerHeight;
+    var pageYOffset = window.pageYOffset;
+    if (windowHeight + pageYOffset >= pageHeight){
+      handleScrollDown(itemsList);
+    }
+};
 
 function handleResponse(response) {
-  document.getElementById("books_list").innerHTML = "";
-  for (var i = 0; i < response.items.length; i++) {
-    var item = response.items[i];
-    // in production code, item.text should have the HTML entities escaped.
-    add_book_to_books_list(item);
+  itemsList = response.items;
+  for (var i = 0; i < 2; i++) {
+    if (typeof(itemsList[i]) == 'undefined') return;
+    add_book_to_books_list(itemsList[i]);
+    book_items_shown += 1;
 }
 }
 
-function shorten_string(str, maxLen, separator = ' ') {
-  if (str){
-  if (str.length <= maxLen) return str;
-  return str.substr(0, str.lastIndexOf(separator, maxLen));
-}
- else return "Brak opisu";
+function handleScrollDown(itemsList) {
+  var book_items_shown_at_beggining = book_items_shown;
+  for (var i = book_items_shown_at_beggining + 1; i < book_items_shown_at_beggining + 3; i++) {
+    if (typeof(itemsList[i]) == 'undefined');
+    add_book_to_books_list(itemsList[i]);
+    book_items_shown += 1;
+  }
 }
 
 function add_book_to_books_list(item){
@@ -48,4 +65,12 @@ function load_searching_script(adjusted_search_input)
   script.src= "https://www.googleapis.com/books/v1/volumes?q=" + adjusted_search_input +"&callback=handleResponse";
   script.id = "current_request_adress";
   searching_scripts_div.appendChild(script);
+}
+
+function shorten_string(str, maxLen, separator = ' ') {
+  if (str){
+  if (str.length <= maxLen) return str;
+  return str.substr(0, str.lastIndexOf(separator, maxLen));
+}
+ else return "Brak opisu";
 }
